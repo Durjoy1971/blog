b993d82c5997a7d76ef08ffc0d4c1bc495feda52
Durjoy-BS23 <durjoy.barua@brainstation-23.com>
2025-12-29 15:45:51 +0600

    Refactor blog structure: add static files directory, update URL patterns, implement post list and detail views, and create base templates with navbar and footer.

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
+    # Blog home page: list of posts
     path('', views.post_list, name='post_list'),
+
+    # Blog detail page: single post
     path('<int:id>/', views.post_detail, name='post_detail'),
-    path('home/', views.post_home, name='post_home'),
 ]
